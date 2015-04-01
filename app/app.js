83678d940d1bdc4105d50d2f4903cf05babbd347
Denise McCoy <Denise.L.McCoy@gmail.com>
2015-03-31 22:07:41 -0600

    added menu

--- app/app.js
+++ app/app.js
@@ -11,6 +11,22 @@ var App = Ember.Application.extend({
   Resolver: Resolver
 });
 
+App.GoogleMapsComponent = Ember.Component.extend({
+  insertMap: function() {
+    var container = this.$(".map-canvas");
+
+    var options = {
+      center: new google.maps.LatLng(this.get("latitude"),
+this.get("longitude")),
+      zoom: 17,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    new google.maps.Map(container[0], options);
+  }.on('didInsertElement')
+});
+
+
 loadInitializers(App, config.modulePrefix);
 
 export default App;
--- config/environment.js
+++ config/environment.js
@@ -13,6 +13,16 @@ module.exports = function(environment) {
       }
     },
 
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-eval' http://maps.googleapis.com/ http://maps.gstatic.com/maps-api-v3/api/js/18/20/main.js",
+      'font-src': "'self'",
+      'connect-src': "'self'",
+      'img-src': "'self'",
+      'style-src': "'self'",
+      'media-src': "'self'"
+    },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
